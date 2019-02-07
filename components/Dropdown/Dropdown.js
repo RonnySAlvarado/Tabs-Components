c39ec4d22c6a41983076e1dc743bbdb0e60ac8b1
Ronny Alvarado <rsalvarado777@gmail.com>
2019-02-07 17:28:37 -0600

    initial commit

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,7 +2,7 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector();
@@ -26,3 +26,4 @@ class Dropdown {
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
